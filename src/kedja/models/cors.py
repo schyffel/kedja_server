33035cd302ea59450bf018c6b91429e547dbf7b0
Robin Harms Oredsson <robin@betahaus.net>
2019-04-12 13:38:16 +0200

    Cors policy, dummy

---
+++ src/kedja/models/cors.py
@@ -0,0 +1,19 @@
+from pyramid.events import NewRequest
+
+
+def add_cors_headers_response_callback(event):
+    def cors_headers(request, response):
+        response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT',
+
+        #'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+       # 'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+       # 'Access-Control-Allow-Credentials': 'true',
+       # 'Access-Control-Max-Age': '1728000',
+        })
+    event.request.add_response_callback(cors_headers)
+
+
+def includeme(config):
+    config.add_subscriber(add_cors_headers_response_callback, NewRequest)
