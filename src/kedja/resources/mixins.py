40d9127b52a1967e809ac374ce91822ca5b4523d
Robin Harms Oredsson <robin@betahaus.net>
2019-04-16 15:44:39 +0200

    integers aren't safe for javascript. Gah

--- src/kedja/resources/mixins.py
+++ src/kedja/resources/mixins.py
@@ -6,4 +6,4 @@ class JSONRenderable(object):
     def __json__(self, request):
         with request.get_mutator(self) as mutator:
             appstruct = mutator.appstruct()
-        return {'type_name': request.registry.content.get_type(self), 'rid': self.rid, 'data': appstruct}
+        return {'type_name': request.registry.content.get_type(self), 'rid': str(self.rid), 'data': appstruct}
