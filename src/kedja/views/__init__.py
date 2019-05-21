f85fe1614fc77e3bedf33627ad4016361df5fe14
Robin Harms Oredsson <robin@betahaus.net>
2019-05-21 15:56:05 +0200

    Cleanup

--- src/kedja/views/__init__.py
+++ src/kedja/views/__init__.py
@@ -1,7 +1,5 @@
 
 def includeme(config):
-#    config.include('._deprecated_api')
-    config.include('._api')
     config.include('.api')
     config.include('.exceptions')
     config.include('.openapi')
