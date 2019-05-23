1ce0de907b99f1b3f8e13c4d83f3f3319a1d0261
Robin Harms Oredsson <robin@betahaus.net>
2019-05-23 11:56:19 +0200

    API request itself needs cors :)

--- src/kedja/views/openapi.py
+++ src/kedja/views/openapi.py
@@ -5,6 +5,7 @@ from cornice.service import get_services, Service
 # Create a service to serve our OpenAPI spec
 swagger = Service(name='OpenAPI',
                   path='/openapi.json',
+                  cors_origins=('*',),
                   description="OpenAPI documentation")
 
 
